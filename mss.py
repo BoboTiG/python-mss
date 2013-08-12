a57456c1d22abdd7fee6607c0a9fada1ea7a9597
BoboTiG <bobotig@gmail.com>
2013-08-12 16:25:20 +0200

    MSSImage class full functional with pure python PNG (filters inside); Add debug informations

--- mss.py
+++ mss.py
@@ -46,7 +46,7 @@ __copyright__ = '''
     in supporting documentation or portions thereof, including
     modifications, that you make.
 '''
-__all__ = ['bgr_to_rgb', 'MSS', 'MSSImage']
+__all__ = ['MSS', 'MSSImage']
 
 
 from ctypes.util import find_library
@@ -80,21 +80,21 @@ if system() == 'Windows':
         WINFUNCTYPE,
         WORD
     )
-    
+
     class BITMAPINFOHEADER(Structure):
-            _fields_ = [
-                ('biSize', DWORD),
-                ('biWidth', LONG),
-                ('biHeight', LONG),
-                ('biPlanes', WORD),
-                ('biBitCount', WORD),
-                ('biCompression', DWORD),
-                ('biSizeImage', DWORD),
-                ('biXPelsPerMeter', LONG),
-                ('biYPelsPerMeter', LONG),
-                ('biClrUsed', DWORD),
-                ('biClrImportant', DWORD)
-            ]
+        _fields_ = [
+            ('biSize',          DWORD),
+            ('biWidth',         LONG),
+            ('biHeight',        LONG),
+            ('biPlanes',        WORD),
+            ('biBitCount',      WORD),
+            ('biCompression',   DWORD),
+            ('biSizeImage',     DWORD),
+            ('biXPelsPerMeter', LONG),
+            ('biYPelsPerMeter', LONG),
+            ('biClrUsed',       DWORD),
+            ('biClrImportant',  DWORD)
+        ]
 
     class BITMAPINFO(Structure):
         _fields_ = [
@@ -114,6 +114,8 @@ class MSS(object):
     Raise an exception if no one found.
     '''
 
+    DEBUG = True
+
     def __init__(self):
         ''' Global vars and class overload. '''
 
@@ -121,20 +123,29 @@ class MSS(object):
         self.oneshot = False
 
         this_is = system()
+        self.debug('__init__', 'system', this_is)
+        self.debug('__init__', 'oneshot', self.oneshot)
+
         if this_is == 'Windows':
             self.__class__ = MSSWindows
         else:
             err = 'System "{0}" not implemented.'.format(this_is)
             raise NotImplementedError(err)
-        
+
         self.init()
+
+    def debug(self, method='', scalar='', value=None):
+        ''' Simple debug output. '''
+
+        if self.DEBUG:
+            print(method + '()', scalar, value)
 
     def save(self, output='mss', oneshot=False, ext='jpg'):
         ''' For each monitor, grab a screen shot and save it to a file.
 
             Parameters:
              - output - string - the output filename without extension
-             - oneshot - c_longean -grab only one screen shot of all monitors
+             - oneshot - boolean -grab only one screen shot of all monitors
              - ext - string - file format to save
 
             This is a generator which returns created files:
@@ -147,7 +158,7 @@ class MSS(object):
         '''
 
         self.oneshot = oneshot
-        self.monitors = self._enum_display_monitors()
+        self.monitors = self._enum_display_monitors() or []
 
         if len(self.monitors) < 1:
             raise ValueError('MSS: no monitor found.')
@@ -155,6 +166,8 @@ class MSS(object):
         # Monitors screen shots!
         i = 1
         for monitor in self.monitors:
+            self.debug('save', 'monitor', monitor)
+
             if self.oneshot:
                 filename = output + '-full'
             else:
@@ -162,7 +175,11 @@ class MSS(object):
                 i += 1
             monitor['width'] = monitor['right'] - monitor['left']
             monitor['height'] = monitor['bottom'] - monitor['top']
+
             pixels = self._get_pixels(monitor)
+            if pixels is None:
+                raise ValueError('MSS: no data to process.')
+
             img = MSSImage(pixels, monitor['width'], monitor['height'])
             img.dump(filename, ext=ext)
             if img.output is not None and isfile(img.output):
@@ -209,15 +226,15 @@ class MSSWindows(MSS):
     ''' Mutli-screen shot implementation for Microsoft Windows. '''
 
     def init(self):
-        ''' Windows related ctypes imports '''
-        
+        ''' Windows initialisations '''
+
         self.SM_XVIRTUALSCREEN = 76
         self.SM_YVIRTUALSCREEN = 77
         self.SM_CXVIRTUALSCREEN = 78
         self.SM_CYVIRTUALSCREEN = 79
         self.SRCCOPY = 0xCC0020
         self.DIB_RGB_COLORS = 0
-        
+
         self.GetSystemMetrics = windll.user32.GetSystemMetrics
         self.EnumDisplayMonitors = windll.user32.EnumDisplayMonitors
         self.GetWindowDC = windll.user32.GetWindowDC
@@ -227,13 +244,13 @@ class MSSWindows(MSS):
         self.BitBlt = windll.gdi32.BitBlt
         self.GetDIBits = windll.gdi32.GetDIBits
         self.DeleteObject = windll.gdi32.DeleteObject
-        
+
         self._set_argtypes()
         self._set_restypes()
-        
+
     def _set_argtypes(self):
         ''' Functions arguments '''
-        
+
         self.MONITORENUMPROC = WINFUNCTYPE(INT, DWORD, DWORD,
                                             POINTER(RECT), DOUBLE)
         self.GetSystemMetrics.argtypes = [INT]
@@ -246,10 +263,10 @@ class MSSWindows(MSS):
         self.DeleteObject.argtypes = [HGDIOBJ]
         self.GetDIBits.argtypes = [HDC, HBITMAP, UINT, UINT, LPVOID,
                                 POINTER(BITMAPINFO), UINT]
-        
+
     def _set_restypes(self):
         ''' Functions return type '''
-        
+
         self.GetSystemMetrics.restypes = INT
         self.EnumDisplayMonitors.restypes = BOOL
         self.GetWindowDC.restypes = HDC
@@ -287,11 +304,11 @@ class MSSWindows(MSS):
         return results
 
     def _get_pixels(self, monitor):
-        ''' Retreive all pixels from a monitor. Pixels have to be RGB. '''        
-        
+        ''' Retreive all pixels from a monitor. Pixels have to be RGB. '''
+
         width, height = monitor['width'], monitor['height']
         left, top = monitor['left'], monitor['top']
-        
+
         srcdc = self.GetWindowDC(0)
         memdc = self.CreateCompatibleDC(srcdc)
         bmp = self.CreateCompatibleBitmap(srcdc, width, height)
@@ -308,21 +325,28 @@ class MSSWindows(MSS):
         pixels = create_string_buffer(buffer_len)
         bits = self.GetDIBits(memdc, bmp, 0, height, byref(pixels),
                                 pointer(bmi), self.DIB_RGB_COLORS)
-        
+
+        self.debug('_get_pixels', 'srcdc', srcdc)
+        self.debug('_get_pixels', 'memdc', memdc)
+        self.debug('_get_pixels', 'bmp', bmp)
+        self.debug('_get_pixels', 'buffer_len', buffer_len)
+        self.debug('_get_pixels', 'bits', bits)
+        self.debug('_get_pixels', 'len(pixels.raw)', len(pixels.raw))
+
         # Clean up
         self.DeleteObject(srcdc)
         self.DeleteObject(memdc)
         self.DeleteObject(bmp)
-        
+
         if bits != height or len(pixels.raw) != buffer_len:
             raise ValueError('MSSWindows: GetDIBits() failed.')
-        
+
         # Note that the origin of the returned image is in the
         # bottom-left corner, 32-bit aligned. Need to "arrange" that.
         #data = bottom_left(pixels.raw, width, height)
         #shift = b' ' * (buffer_len - len(data))
         #data.extend(shift)
-        
+
         # We need to convert BGR to RGB
         #return bgr_to_rgb(bytes(scanlines), width, height)
         data = pixels.raw
@@ -364,9 +388,9 @@ class MSSImage(object):
         exts.append('jpg, png, ..., it will try using external libraries')
         return exts
 
-    def dump(self, output=None, ext='jpg', quality=80):
+    def dump(self, output=None, ext='jpg', quality=95):
         ''' Dump data to the image file using file format specified by
-            ext. 
+            ext.
             To save time, it will try if PIL or another image library is
             available to use or it will use the wrapper to libjpeg using ctypes.
             Finally, if all fails, it will use the pure python PNG implementation.
@@ -378,36 +402,35 @@ class MSSImage(object):
         #    raise ValueError('MSSImage: bad data [{0}].'.format(type(self.data)))
         #if not ext in self.ext_ok:
         #    raise ValueError('MSSImage: unknown extension.')
-        if quality < 1:
-            raise ValueError('MSSImage: quality must be positive.')
 
         self.filename = output
         self.ext = ext
-        self.quality = min(quality, 100)
+        self.quality = max(0, min(quality, 100))
         self.output = None
         contents = None
 
         # Pure python implementations start with 'lib' (libjpg, libpng)
         if hasattr(self, self.ext):
             contents = getattr(self, self.ext)()
-        
+
         # Try external libraries first
         if contents is None:
             try:
                 buffer_len = (self.width * 3 + 3) & -4
                 from PIL import Image, ImageFile
                 tmp = self.filename + '.' + self.ext
+                #img = Image.frombuffer('RGB', (self.width, self.height),
+                #                        self.data, 'raw', 'BGR', buffer_len, -1)
                 img = Image.frombuffer('RGB', (self.width, self.height),
                                         self.data, 'raw', 'RGB', buffer_len, 1)
                 ImageFile.MAXBLOCK = self.width * self.height
-                #img.save(tmp, quality=self.quality, optimize=True, progressive=True)
-                img.save(tmp, quality=self.quality)
+                img.save(tmp, quality=self.quality, optimize=True, progressive=True)
                 self.output = tmp
                 return
             except Exception as ex:
                 print(ex)
                 pass
-        
+
         # Here, no success, use pure python PNG implementation
         #if contents is None:
         #    contents = self.libpng()
@@ -429,14 +452,109 @@ class MSSImage(object):
             http://inaps.org/journal/comment-fonctionne-le-png
         '''
 
+        def filter_scanline(ftype, line, fo, prev=None):
+            ''' http://pypng.googlecode.com/svn/trunk/code/png.py
+                Apply a scanline filter to a scanline. `ftype` specifies the
+                filter type (0 to 4); `line` specifies the current (unfiltered)
+                scanline as a sequence of bytes; `prev` specifies the previous
+                (unfiltered) scanline as a sequence of bytes. `fo` specifies the
+                filter offset; normally this is size of a pixel in bytes (the number
+                of bytes per sample times the number of channels), but when this is
+                < 1 (for bit depths < 8) then the filter offset is 1.
+            '''
+
+            scanline = pack(b'B', ftype)
+
+            def sub():
+                out = b''
+                ai = -fo
+                for x in line:
+                    if ai >= 0:
+                        x = (x - line[ai]) & 0xff
+                    out += pack(b'B', x)
+                    ai += 1
+                return out
+
+            def up():
+                out = b''
+                for i, x in enumerate(line):
+                    x = (x - prev[i]) & 0xff
+                    out += pack(b'B', x)
+                return out
+
+            def average():
+                out = b''
+                ai = -fo
+                for i, x in enumerate(line):
+                    if ai >= 0:
+                        x = (x - ((line[ai] + prev[i]) >> 1)) & 0xff
+                    else:
+                        x = (x - (prev[i] >> 1)) & 0xff
+                    out += pack(b'B', x)
+                    ai += 1
+                return out
+
+            def paeth():
+                out = b''
+                ai = -fo
+                for i, x in enumerate(line):
+                    a = 0
+                    b = prev[i]
+                    c = 0
+
+                    if ai >= 0:
+                        a = line[ai]
+                        c = prev[ai]
+                    p = a + b - c
+                    pa = abs(p - a)
+                    pb = abs(p - b)
+                    pc = abs(p - c)
+                    if pa <= pb and pa <= pc: Pr = a
+                    elif pb <= pc: Pr = b
+                    else: Pr = c
+
+                    x = (x - Pr) & 0xff
+                    out += pack(b'B', x)
+                    ai += 1
+                return out
+
+            if not prev:
+                # We're on the first line.  Some of the filters can be reduced
+                # to simpler cases which makes handling the line "off the top"
+                # of the image simpler.  "up" becomes "none"; "paeth" becomes
+                # "left" (non-trivial, but true). "average" needs to be handled
+                # specially.
+                if ftype == 2: # "up"
+                    return bytes(line) # type = 0
+                elif ftype == 3:
+                    prev = [0] * len(line)
+                elif ftype == 4: # "paeth"
+                    ftype = 1
+            if ftype == 0:
+                scanline += bytes(line)
+            elif ftype == 1:
+                scanline += sub()
+            elif ftype == 2:
+                scanline += up()
+            elif ftype == 3:
+                scanline += average()
+            else: # type == 4
+                scanline += paeth()
+            return scanline
+
         self.ext = 'png'
         width, height, data = self.width, self.height, self.data
-        
+
         to_take = (width * 3 + 3) & -4
+        padding = (to_take % 32) // 2  # Fix for MS Windows 64 bits
         offset = 0
         scanlines = b''
+        filter_type = 4
+        prev = None
         for y in range(height):
-            scanlines += b'\0' + data[offset:offset + to_take]
+            line = bytearray(data[offset : offset+to_take-padding])
+            scanlines += filter_scanline(filter_type, line, 3, prev)
+            prev = line
             offset += to_take
 
         magic = pack(b'>8B', 137, 80, 78, 71, 13, 10, 26, 10)
@@ -450,7 +568,6 @@ class MSSImage(object):
         # Data: size, marker, data, CRC32
         idat = [b'', b'IDAT', b'', b'']
         idat[2] = zlib.compress(scanlines, 9)
-        #idat[2] = scanlines
         idat[3] = pack(b'>I', zlib.crc32(b''.join(idat[1:3])) & 0xffffffff)
         idat[0] = pack(b'>I', len(idat[2]))
 
@@ -458,14 +575,14 @@ class MSSImage(object):
         iend = [b'', b'IEND', b'', b'']
         iend[3] = pack(b'>I', zlib.crc32(iend[1]) & 0xffffffff)
         iend[0] = pack(b'>I', len(iend[2]))
-        
+
         return magic + b''.join(ihdr) + b''.join(idat) + b''.join(iend)
 
 
 def bottom_left(data, width, height):
     ''' Reorganise data when the origin of the image is in the
         bottom-left corner. '''
-    
+
     scanlines = bytearray()
     y = height - 1
     while y >= 0:
@@ -492,51 +609,60 @@ if __name__ == '__main__':
     try:
         mss = MSS()
         # One screen shot per monitor
-        for filename in mss.save():
-            print('File "{0}" created.'.format(filename))
-        for filename in mss.save(ext='libpng'):
-            print('File "{0}" created.'.format(filename))
-
-        # A screen shot to grab them all :)
+        #for filename in mss.save():
+        #    print('File "{0}" created.'.format(filename))
         #for filename in mss.save(oneshot=True):
         #    print('File "{0}" created.'.format(filename))
+
+        # A screen shot to grab them all :)
+        for filename in mss.save(oneshot=True):
+            print('File "{0}" created.'.format(filename))
     except Exception as ex:
-        print(ex)
-        #raise
-        
+        #print(ex)
+        raise
+
     '''
     # MSSImage tests
     try:
+        from os import path
         img = MSSImage()
 
         img.width = 1440
         img.height = 900
         with open('data.raw', 'rb') as fileh:
             data = fileh.read()
-            
+
             img.data = data
-            img.dump('data-output-pil', ext='png', quality=100)
-            print('File "{0}" created.'.format(img.output))
-            
+            img.dump('data-output-pil', ext='png', quality=95)
+            if img.output is not None:
+                statinfo = path.getsize(img.output)
+                print('File "{0}" created: {1}'.format(img.output, statinfo))
+
             img.data = data
             img.dump('data-output', ext='libpng')
-            print('File "{0}" created.'.format(img.output))
-        
+            if img.output is not None:
+                statinfo = path.getsize(img.output)
+                print('File "{0}" created: {1}'.format(img.output, statinfo))
+
         print("")
-        
+
         img.width = 1366
         img.height = 768
         with open('data2.raw', 'rb') as fileh:
             data = fileh.read()
-            
+
             img.data = data
-            img.dump('data2-output-pil', ext='png', quality=100)
-            print('File "{0}" created.'.format(img.output))
-            
+            img.dump('data2-output-pil', ext='png', quality=95)
+            if img.output is not None:
+                statinfo = path.getsize(img.output)
+                print('File "{0}" created: {1}'.format(img.output, statinfo))
+
             img.data = data
             img.dump('data2-output', ext='libpng')
-            print('File "{0}" created.'.format(img.output))
+            if img.output is not None:
+                statinfo = path.getsize(img.output)
+                print('File "{0}" created: {1}'.format(img.output, statinfo))
     except Exception as ex:
         print(ex)
         raise
-    '''
+    #'''
