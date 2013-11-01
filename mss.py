fa0613b84df5702ea1e2d2f7b2c99d0e23ce7246
BoboTiG <bobotig@gmail.com>
2013-11-01 21:10:51 +0100

    MSSImage: fix an error and code review

--- mss.py
+++ mss.py
@@ -709,7 +709,7 @@ class MSSImage(object):
     '''
 
     def __init__(self, data=None, width=1, height=1):
-        if self.data is None:
+        if data is None:
             raise ValueError('MSSImage: no data to process.')
         elif width < 1 or height < 1:
             raise ValueError('MSSImage: width or height must be positive.')
@@ -725,31 +725,31 @@ class MSSImage(object):
             http://inaps.org/journal/comment-fonctionne-le-png
         '''
 
-        to_take = (self.width * 3 + 3) & -4
-        padding = 0 if to_take % 8 == 0 else (to_take % 8) // 2
-        height, data = self.height, self.data
-        scanlines = [b''.join([b'0', data[to_take*y:to_take*y+to_take-padding]]) for y in range(height)]
-
-        magic = pack(b'>8B', 137, 80, 78, 71, 13, 10, 26, 10)
-
-        # Header: size, marker, data, CRC32
-        ihdr = [b'', b'IHDR', b'', b'']
-        ihdr[2] = pack(b'>2I5B', self.width, self.height, 8, 2, 0, 0, 0)
-        ihdr[3] = pack(b'>I', zlib.crc32(b''.join(ihdr[1:3])) & 0xffffffff)
-        ihdr[0] = pack(b'>I', len(ihdr[2]))
-
-        # Data: size, marker, data, CRC32
-        idat = [b'', b'IDAT', b'', b'']
-        idat[2] = zlib.compress(b''.join(scanlines), 9)
-        idat[3] = pack(b'>I', zlib.crc32(b''.join(idat[1:3])) & 0xffffffff)
-        idat[0] = pack(b'>I', len(idat[2]))
-
-        # Footer: size, marker, None, CRC32
-        iend = [b'', b'IEND', b'', b'']
-        iend[3] = pack(b'>I', zlib.crc32(iend[1]) & 0xffffffff)
-        iend[0] = pack(b'>I', len(iend[2]))
-
         with open(output, 'wb') as fileh:
+            to_take = (self.width * 3 + 3) & -4
+            padding = 0 if to_take % 8 == 0 else (to_take % 8) // 2
+            height, data = self.height, self.data
+            scanlines = [b''.join([b'0', data[to_take*y:to_take*y+to_take-padding]]) for y in range(height)]
+
+            magic = pack(b'>8B', 137, 80, 78, 71, 13, 10, 26, 10)
+
+            # Header: size, marker, data, CRC32
+            ihdr = [b'', b'IHDR', b'', b'']
+            ihdr[2] = pack(b'>2I5B', self.width, self.height, 8, 2, 0, 0, 0)
+            ihdr[3] = pack(b'>I', zlib.crc32(b''.join(ihdr[1:3])) & 0xffffffff)
+            ihdr[0] = pack(b'>I', len(ihdr[2]))
+
+            # Data: size, marker, data, CRC32
+            idat = [b'', b'IDAT', b'', b'']
+            idat[2] = zlib.compress(b''.join(scanlines), 9)
+            idat[3] = pack(b'>I', zlib.crc32(b''.join(idat[1:3])) & 0xffffffff)
+            idat[0] = pack(b'>I', len(idat[2]))
+
+            # Footer: size, marker, None, CRC32
+            iend = [b'', b'IEND', b'', b'']
+            iend[3] = pack(b'>I', zlib.crc32(iend[1]) & 0xffffffff)
+            iend[0] = pack(b'>I', len(iend[2]))
+
             fileh.write(magic + b''.join(ihdr) + b''.join(idat) + b''.join(iend))
             return output
         return None
