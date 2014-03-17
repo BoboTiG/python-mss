f98d616c33906879dead4537499d8ab7f25a6831
BoboTiG <bobotig@gmail.com>
2014-03-17 15:01:26 +0100

    Update setup.py with 3.4 Python version ready

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Multimedia :: Graphics :: Capture :: Screen Capture',
     ],
     url='https://github.com/BoboTiG/python-mss'
