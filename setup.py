47d1e19b110f2d51b6bd848b41be39ce4ed3513a
BoboTiG <bobotig@gmail.com>
2015-04-13 23:13:46 +0200

    Python 3.5 ready

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Multimedia :: Graphics :: Capture :: Screen Capture',
     ],
     url='https://github.com/BoboTiG/python-mss'
