c70b30a34c6f023504f328b7595bf72c76f649c6
BoboTiG <bobotig@gmail.com>
2015-04-13 23:36:24 +0200

    Update Python versions

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         'License :: OSI Approved :: zlib/libpng License',
         'Natural Language :: English',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
