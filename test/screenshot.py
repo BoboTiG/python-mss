5c95d1ffa501bf06736138c9f19bf7b0b05773ab
BoboTiG <bobotig@gmail.com>
2015-05-01 16:11:24 +0200

    tests: fix typo

--- test/screenshot.py
+++ test/screenshot.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
-''' A simple screen shot script.
+''' A simple screenshot script using the MSS module.
     Use of python-mss: https://github.com/BoboTiG/python-mss/
+
     pip install --upgrade mss
 '''
 
-from platform import system
+from __future__ import print_function
+
 from mss import mss
 
-if __name__ == '__main__':
-    screnshotter = mss()
-    for filename in screnshotter.save(output='monitor-%d.png', screen=1):
-        print('File: "{0}" created.'.format(filename))
+
+screenshotter = mss()
+for filename in screenshotter.save(output='monitor-%d.png', screen=1):
+    print('File: "{0}" created.'.format(filename))
