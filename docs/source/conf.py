2b9673a35ee51915d27349ff9bb4602411fdf71a
Tiger-222 <contact@tiger-222.fr>
2016-10-14 18:37:58 +0200

    New contributor: Jochen 'cycomanic' Schroeder

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,7 +59,7 @@ author = 'Tiger-222'
 # built documents.
 #
 # The short X.Y version.
-version = '2.0.13'
+version = '2.0.14'
 # The full version, including alpha/beta/rc tags.
 release = 'latest'
 
--- mss/__init__.py
+++ mss/__init__.py
@@ -13,7 +13,7 @@
 from .exception import ScreenshotError
 from .factory import mss
 
-__version__ = '2.0.13'
+__version__ = '2.0.14'
 __author__ = "Mickaël 'Tiger-222' Schoentgen"
 __copyright__ = '''
     Copyright (c) 2013-2016, Mickaël 'Tiger-222' Schoentgen
