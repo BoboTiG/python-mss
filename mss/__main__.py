2e9dfe2fe4dbea3fbdf964e45846e6e5f90e3178
Mickaël Schoentgen <mschoentgen@nuxeo.com>
2017-11-23 18:43:05 +0100

    MSS: Add the 'mss' entry point

--- mss/__main__.py
+++ mss/__main__.py
@@ -6,6 +6,10 @@ Source: https://github.com/BoboTiG/python-mss
 
 from __future__ import print_function
 
+import sys
+from argparse import ArgumentParser
+
+from . import __version__
 from .exception import ScreenShotError
 from .factory import mss
 
@@ -14,9 +18,22 @@ def main():
     # type: () -> int
     """ Main logic. """
 
+    cli_args = ArgumentParser()
+    cli_args.add_argument('-m', '--monitor', default=0, type=int,
+                          help='the monitor to screenshot')
+    cli_args.add_argument('-o', '--output', default='monitor-%d.png',
+                          help='the output file name')
+    cli_args.add_argument('-v', '--version', action='version',
+                          version=__version__)
+    parser = cli_args.parse_args(sys.argv[1:])
+    kwargs = {
+        'mon': parser.monitor,
+        'output': parser.output,
+    }
+
     try:
         with mss() as sct:
-            for file_name in sct.save():
+            for file_name in sct.save(**kwargs):
                 print(file_name)
             return 0
     except ScreenShotError:
