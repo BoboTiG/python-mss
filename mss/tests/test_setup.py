40518940e20a4d695c9ec5fd4d53294cbbace696
Mickaël Schoentgen <contact@tiger-222.fr>
2022-10-27 14:55:12 +0200

    test: fix test_wheel_python_3_only()

--- mss/tests/test_setup.py
+++ mss/tests/test_setup.py
@@ -2,21 +2,25 @@
 This is part of the MSS Python's module.
 Source: https://github.com/BoboTiG/python-mss
 """
+import platform
 from subprocess import STDOUT, check_call, check_output
+
+import pytest
 
 from mss import __version__
 
-INSTALL = "python -m build --sdist --wheel".split()
+if platform.system().lower() != "linux":
+    pytestmark = pytest.mark.skip
+
+# Note: using `--no-isolation` because it doesn't work with `tox`
+INSTALL = "python -m build --no-isolation --sdist --wheel".split()
 CHECK = "twine check dist/*".split()
 
 
 def test_wheel_python_3_only():
     """Ensure the produced wheel is Python 3 only."""
-    output = check_output(INSTALL, stderr=STDOUT, text=True)
-    text = f"mss-{__version__}-py3-none-any.whl"
+    output = str(check_output(INSTALL, stderr=STDOUT))
+    text = f"Successfully built mss-{__version__}.tar.gz and mss-{__version__}-py3-none-any.whl"
     assert text in output
-
-    print(output)
-    assert "warning" not in output.lower()
 
     check_call(CHECK)
