abc4ecd8a2b5d1275c1289423e6713219a9c876a
Halldor Fannar <halldorfannar@users.noreply.github.com>
2026-01-18 14:08:30 +0000

    chore(dev): add check.ps1 for Windows (#459)
    Update docs as well.

---
+++ check.ps1
@@ -0,0 +1,10 @@
+#
+# Small script to ensure quality checks pass before submitting a commit/PR.
+#
+$ErrorActionPreference = "Stop"
+
+python -m ruff format docs src
+python -m ruff check --fix --unsafe-fixes docs src
+
+# "--platform win32" to not fail on ctypes.windll (it does not affect the overall check on other OSes)
+python -m mypy --platform win32 src docs/source/examples
